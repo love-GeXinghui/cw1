ca1f778b5a4a10e35b27f8978b12373ca51c3ddf
zhaoyanbo <sc19y3z@leeds.ac.uk>
2021-03-16 13:23:54 +0800

    添加项目文件。

---
+++ main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void makeMenu()
+{
+	printf("-----------------------------------------------\n");
+	printf("   Welcome to the library management system!\n");
+	printf("\t0.quit");
+	printf("\t1.registration books\n");
+	printf("\t2.browse books\n");
+	printf("\t3.borrow books\n");
+	printf("\t4.return books\n");
+	printf("\t5.store  books\n");
+	printf("\t5.store  books\n");
+	printf("\t6.delete books\n");
+	printf("\t7.find   books\n");
+	printf("     Please input number(0-7):");
+	printf("\n----------------------------------------------");
+}
+
+void keyDown() {
+
+}
+int main()
+{
+	makeMenu();
+}
